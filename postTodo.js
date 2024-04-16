3805a168a8fb84662311c04b3edc53b76be347e5
Tippy Wang <ektodorwang@gmail.com>
2024-04-16 20:55:53 +0800

    新增POST

---
+++ postTodo.js
@@ -0,0 +1,26 @@
+const Todos = require('./model/todo');
+const successHandle = require('./successHandle');
+const errorHandle = require('./errorHandle');
+const { v4: uuidv4 } = require('uuid');
+
+function postTodo(req, res) {
+  let body = '';
+  req.on('data', (chunk) => {
+    body += chunk;
+  });
+
+  req.on('end', async () => {
+    try {
+      const title = JSON.parse(body)?.title;
+      const newTodo = await Todos.create({
+        title,
+      });
+
+      successHandle(res, newTodo);
+    } catch (error) {
+      errorHandle(res);
+    }
+  });
+}
+
+module.exports = postTodo;
