2476828b73706e80037b5bffc845db499a988624
sunny96087 <sunny96087@yahoo.com.tw>
2024-04-16 20:50:01 +0800

    新增 get todo

---
+++ getTodo.js
@@ -0,0 +1,14 @@
+const Todos = require('./models/todo');
+const successHandle = require('./successHandle');
+const errorHandle = require('./errorHandle');
+
+const getTodo = async (res) => {
+  try {
+    const todos = await Todos.find();
+    successHandle(res, todos);
+  } catch (error) {
+    errorHandle(res);
+  }
+}
+
+module.exports = getTodo;
