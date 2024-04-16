d382f53286c9fd1f5f91c5e45f4439c49b3c6d9a
shuantt <tehsuan.tht@gmail.com>
2024-04-16 21:18:47 +0800

    feat : test

--- getTodo.js
+++ getTodo.js
@@ -1,14 +1,1 @@
-const Todos = require('./models/todo');
-const successHandle = require('./successHandle');
-const errorHandle = require('./errorHandle');
-
-const getTodo = async (res) => {
-  try {
-    const todos = await Todos.find();
-    successHandle(res, todos);
-  } catch (error) {
-    errorHandle(res);
-  }
-}
-
-module.exports = getTodo;
+//hihi
