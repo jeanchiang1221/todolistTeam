d472e4951baa8376a41d99450994e9de698258bb
sunny96087 <sunny96087@yahoo.com.tw>
2024-04-17 09:13:02 +0800

    將所有 api 連到 server

--- server.js
+++ server.js
@@ -1,36 +1,43 @@
 const http = require('http');
 const { v4: uuidv4 } = require('uuid');
-const errHandle = require('./errorHandle');
-const todos = [];
+const mongoose = require('mongoose');
+const headers = require('./headersSetting');
+const successHandle = require('./successHandle');
+const postTodo = require('./postTodo');
 const getTodo = require('./getTodo');
+const patchTodo = require('./patchTodo');
+const deleteTodo = require('./deleteTodo');
+
+dotenv.config({ path: './config.env' })
+
+const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
+mongoose
+  .connect(DB) // 連線資料庫
+  .then(() => {
+    console.log('資料庫連線成功');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const requestListener = (req, res)=>{
-    const headers = {
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
-        'Content-Type': 'application/json'
-    }
-    let body = "";
-    
-    req.on('data', chunk=>{
-        body+=chunk;
-    })
-    
     if(req.url=="/todos" && req.method == "GET"){
         // getTodo.js
         getTodo(res);
     }else if(req.url=="/todos" && req.method == "POST"){
         // postTodo.js
+        postTodo(res, req);
     }else if(req.url=="/todos" && req.method == "DELETE"){
         // deleteTodo.js
+        deleteTodo(res);
     }else if(req.url.startsWith("/todos/") && req.method=="DELETE"){
         // deleteTodo.js
+        deleteTodo(res, req);
     }else if(req.url.startsWith("/todos/") && req.method=="PATCH"){
         // patchTodo.js
+        patchTodo(res, req);
     }else if(req.method == "OPTIONS"){
-        res.writeHead(200,headers);
-        res.end();
+        successHandle(res);
     }else{
         res.writeHead(404,headers);
         res.write(JSON.stringify({
